2998f3194277a8ea079a7f8480f60e202f26faed
jooaogbriel <joao.ggwp@outlook.com>
2024-09-21 11:13:38 -0300

    feat: create initial tests to [productService]

---
+++ src/service/product.service.spec.js
@@ -0,0 +1,11 @@
+describe("Product service unit tests", () => {
+  it("should change the prices of all products", () => {
+    const product1 = new Product("product1", "Product 1", 10);
+    const product2 = new Product("product2", "Product 2", 20);
+    const products = [product1, product2];
+
+    ProductService.increasePrice(products, 100);
+    expect(product1.price).toStrictEqual(20);
+    expect(product2.price).toStrictEqual(40);
+  });
+});
