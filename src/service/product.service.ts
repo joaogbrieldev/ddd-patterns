7642345048c6e601c16d3c9641539ef12911e212
jooaogbriel <joao.ggwp@outlook.com>
2024-09-24 19:23:29 -0300

    feat: create orderService
      i created orderService class for domain specific methods

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -2,8 +2,8 @@ import Product from "../entity/product";
 
 export class ProductService {
   static increasePrice(products: Product[], percentagem: number): void {
-    products.forEach(
-      (product) => (product.price * percentagem) / 100 + product.price
-    );
+    products.forEach((product) => {
+      product.changePrice((product.price * percentagem) / 100 + product.price);
+    });
   }
 }
