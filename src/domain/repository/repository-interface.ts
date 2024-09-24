84db21154eacb0f6fa896ca08935034174e0fa42
jooaogbriel <joao.ggwp@outlook.com>
2024-09-24 19:40:29 -0300

    feat: init repository interface

--- src/domain/repository/repository-interface.ts
+++ src/domain/repository/repository-interface.ts
@@ -1,5 +1,6 @@
 export default interface IRepository<T> {
   create(entity: T): Promise<void>;
   update(entity: T): Promise<void>;
-  update(id: string): Promise<T>;
+  find(id: string): Promise<T>;
+  findAll(id: string): Promise<T[]>;
 }
