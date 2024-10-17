e8ba2c43e18b0b0517f3889a63baedcff565b5ca
João Figueiredo <joao.ggwp@outlook.com>
2024-10-17 19:14:12 -0300

    feat: create `event dispacher`
      i created event dispacher with register method and get all event handlers with tests

---
+++ src/domain/event/@shared/product/product-created.event.ts
@@ -0,0 +1,11 @@
+import IEvent from "../event.interface";
+
+export default class ProductCreatedEvent implements IEvent {
+  dataTimeOccured: Date;
+  eventData: any;
+  
+  constructor (eventData: any) {
+    this.dataTimeOccured = new Date();
+    this.eventData = eventData;
+  }
+}
