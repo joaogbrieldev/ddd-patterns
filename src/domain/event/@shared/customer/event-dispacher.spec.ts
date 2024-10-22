6cf7d7dae8d29280697754689f6a01a9c46574b7
João Figueiredo <joao.ggwp@outlook.com>
2024-10-22 19:21:32 -0300

    feat: create two `events`
     event to create customer and event when address in customer has changed

---
+++ src/domain/event/@shared/customer/event-dispacher.spec.ts
@@ -0,0 +1,30 @@
+import Address from "../../../../value-object/address"
+import Customer from "../../../entity/customer/customer"
+import EventDispatcher from "../event-dispacher"
+import { SendConsoleLogWhenAddressIsChangedHandler, SendEmailWhenCustomerIsCreatedHandler } from "./handler/send-msg.handler"
+
+describe("create customer tests", () => {
+
+  test('should register an event handler', () => {
+    const eventDispacher = new EventDispatcher()
+    const eventHandler = new SendEmailWhenCustomerIsCreatedHandler()
+    eventDispacher.register('CustomerCreated',eventHandler)
+    eventDispacher.getEventHandlers["CustomerCreated"]
+    expect( eventDispacher.getEventHandlers["CustomerCreated"].length).toBe(1)
+  })
+  test('should register an event handler', () => {
+    const eventDispacher = new EventDispatcher()
+    const customer = new Customer('any_id', 'any_name')
+    const newAddress = new Address(
+      "any",
+      1,
+      "any",
+      "any",
+    )
+    customer.changeAddress(newAddress)
+    const eventHandler = new SendConsoleLogWhenAddressIsChangedHandler()
+    eventDispacher.register('AdressChanged',eventHandler)
+    eventDispacher.getEventHandlers["AdressChanged"]
+    expect( eventDispacher.getEventHandlers["AdressChanged"].length).toBe(1)
+  })
+})
